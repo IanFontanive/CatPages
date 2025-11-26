ada720b57c6032c21f2dca0d073065e922cf20ee
IanFontanive <98621718+IanFontanive@users.noreply.github.com>
2025-11-26 14:14:22 -0300

    init: Configuração inicial da aplicação

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/quem_somos')
+def quem_somos():
+    return render_template('quem_somos.html')
+
+@app.route('/gato_salem')
+def gato_salem():
+    return render_template('gato_salem.html')
+
+@app.route('/gato_espoleta')
+def gato_espoleta():
+    return render_template('gato_espoleta.html')
+
+@app.route('/gato_pipoca')
+def gato_pipoca():
+    return render_template('gato_pipoca.html')
+
+@app.route('/gato_lola')
+def gato_lola():
+    return render_template('gato_lola.html')
+
+app.run()
